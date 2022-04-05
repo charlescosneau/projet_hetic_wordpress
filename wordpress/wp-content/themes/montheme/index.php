776fa80a8a6c46768d7253c4fd2e9c37f3580943
BastianPEIRE <bastian.peire@gmail.com>
2022-04-05 09:36:03 +0200

    grid style

--- wordpress/wp-content/themes/montheme/index.php
+++ wordpress/wp-content/themes/montheme/index.php
@@ -12,21 +12,19 @@
 <div class="container-all-card">
     <div class="view-container">
         <?php if (have_posts()) : ?>
-            <div>
-                <?php while (have_posts()) : the_post(); ?>
-                    <div class="card">
-                        <h5 class="card-title"><?php the_title() ?></h5>
-                        <?php the_post_thumbnail('card-header', ['class' => 'card_img_top', 'alt' => '']) ?>
-                        <h6 class="card-category"><?php the_category() ?></h6>
-                        <p class="card-text"><?php the_content('En voir plus') ?></p>
-                        <p><?php the_terms(get_the_ID(), 'recette') ?></p>
-                        <button>
-                            <a class="card-link" href="<?php the_permalink() ?>">Voir plus</a>
-                        </button>
-                    </div>
-                <?php endwhile ?>
-                <?php montheme_pagination() ?>
-            </div>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="card">
+                    <h5 class="card-title"><?php the_title() ?></h5>
+                    <?php the_post_thumbnail('card-header', ['class' => 'card_img_top', 'alt' => '']) ?>
+                    <h6 class="card-category"><?php the_category() ?></h6>
+                    <p class="card-text"><?php the_content('En voir plus') ?></p>
+                    <p><?php the_terms(get_the_ID(), 'recette') ?></p>
+                    <button>
+                        <a class="card-link" href="<?php the_permalink() ?>">Voir plus</a>
+                    </button>
+                </div>
+            <?php endwhile ?>
+            <?php montheme_pagination() ?>
         <?php else : ?>
             <h1>Pas d'articles</h1>
         <?php endif; ?>
